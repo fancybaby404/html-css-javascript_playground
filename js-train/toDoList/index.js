3909489b1f83195438f44cc6e3b2f66bcb77fa3d
Julian-PC <julianbuckham404@gmail.com>
2021-10-20 23:06:07 +0800

    themes are working (light and dark mode)todo:photo

--- js-train/toDoList/index.js
+++ js-train/toDoList/index.js
@@ -1,4 +1,35 @@
+const checkTheme = () => {
+    if (localStorage.getItem('theme') == 'light') {
+        // remove theme class
+        $('body').removeClass('dark-theme')
+        $('body').removeClass('photo-theme')
+
+        // add theme class
+        $('body').addClass('light-theme')
+        return;
+    } else if (localStorage.getItem('theme') == 'dark') {
+        // remove theme class
+        $('body').removeClass('light-theme')
+        $('body').removeClass('photo-theme')
+
+        // add theme class
+        $('body').addClass('dark-theme')
+        return;
+    } else if (localStorage.getItem('theme') == 'photo') {
+        // remove theme class
+        $('body').removeClass('light-theme')
+        $('body').removeClass('dark-theme')
+
+        // add theme class
+        $('body').addClass('photo-theme')
+        return;
+    }
+}
+
+
 jQuery(() => {
+    checkTheme();
+
     // get current todo stuff
     let appendDis = localStorage.getItem('todo')
     $('ul').append(appendDis)
@@ -12,23 +43,28 @@ jQuery(() => {
     // =======================
 
     // length of todo
-    let todoLength = $('li').length
+    let todoLength = $('.todo li').length
     $('h1')[0].innerHTML = `You have ${todoLength} tasks today`
 
-    // ========== save current list of todo
+    // ========== save current list of todo/theme
     $('body').on('click', (event) => {
+        // save todo-items
         localStorage.setItem('todo', $('ul')[0].innerHTML)
     })
 
-    // ========== remove todo button
-    $('body').on('click', '.rmv', (event) => {
-        // remove 
-        $(event.target).parent()[0].remove()
+    // ========== Theme
+    $('input').on('click', () => {
+        // save theme
+        if ($('#Light').is(':checked')) {
+            localStorage.setItem('theme', 'light')
+        } else if ($('#Dark').is(':checked')) {
+            localStorage.setItem('theme', 'dark')
+        } else if ($('#Photo').is(':checked')) {
+            localStorage.setItem('theme', 'photo')
+        }
 
-        // change todolength
-        let todoLength = $('li').length
-        $('h1')[0].innerHTML = `You have ${todoLength} tasks today`
-    });
+        checkTheme();
+    })
 
     // ========== add todo button
     $('.add').on('click', (event) => {
@@ -49,19 +85,60 @@ jQuery(() => {
         }
 
         // change todolength
-        let todoLength = $('li').length
+        let todoLength = $('.todo li').length
         $('h1')[0].innerHTML = `You have ${todoLength} tasks today`
 
     });
 
+    // ========== remove todo button
+    $('body').on('click', '.rmv', (event) => {
+        // remove 
+        $(event.target).parent()[0].remove()
+
+        // change todolength
+        let todoLength = $('.todo li').length
+        $('h1')[0].innerHTML = `You have ${todoLength} tasks today`
+    });
+
     // ========== show settigns
     $('.settings-icon').on('click', (event) => {
-        let attr = $('.settings').attr('hidden');
-        if (typeof attr !== 'undefined' && attr !== false) {
-            $('.settings').attr('hidden', false)
-        } else {
-            $('.settings').attr('hidden', true)
-        }
+
+        // if ($('.settings').position().top >= 1000) {
+        //     console.log('going up')
+        //     $('.settings-icon').addClass('disabled')
+        //     $('.settings').animate({ top: '0px' }, 1500);
+        //     $('.settings-icon').removeClass('disabled')
+
+        // } else {
+        //     console.log('going down')
+        //     $('.settings-icon').addClass('disabled')
+        //     $('.settings').animate({ top: '1000px' }, 1500);
+        //     $('.settings-icon').removeClass('disabled')
+        // }
+
+        // if ($('.settings').hasClass('slide-up')) {
+        //     $('.settings').addClass('slide-down', 1000, 'easeOutBounce');
+        //     $('.settings').removeClass('slide-up');
+        // } else {
+        //     $('.settings').removeClass('slide-down');
+        //     $('.settings').addClass('slide-up', 1000, 'easeOutBounce');
+        // }
+
+        // let attr = $('.settings').attr('hidden');
+        $('.settings').fadeToggle()
+
+        // if (typeof attr !== 'undefined' && attr !== false) {
+        //     setTimeout(() => {
+        //         $('.settings').slideUp()
+        //     }, 5000)
+        //     $('.settings').attr('hidden', false)
+        // } else {
+        //     setTimeout(() => {
+        //         $('.settings').slideDown()
+        //     }, 5000)
+        //     $('.settings').attr('hidden', true)
+        // }
+
     })
 
     // sortable todo
